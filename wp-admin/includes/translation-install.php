eef78a6de5e95fd9440c73abc1e59abf8cd00ea0
Mirza Hussain Nazari <mirzanazari77@gmail.com>
2022-09-29 16:01:28 +0330

    SeChanges

--- wp-admin/includes/translation-install.php
+++ wp-admin/includes/translation-install.php
@@ -65,7 +65,7 @@ function translations_api( $type, $args = null ) {
 					__( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="%s">support forums</a>.' ),
 					__( 'https://wordpress.org/support/forums/' )
 				) . ' ' . __( '(WordPress could not establish a secure connection to WordPress.org. Please contact your server administrator.)' ),
-				headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE
+				//headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE
 			);
 
 			$request = wp_remote_post( $http_url, $options );
